21f2ddfcdf43398e004c09b7cba285d6937494b8
wb-tf707260 <wb-tf707260@alibaba-inc.com>
2020-09-27 12:51:57 +0800

    Signed-off-by: wb-tf707260 <wb-tf707260@alibaba-inc.com>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+//项目生产环境打包时候的配置
+const prodDir = 'data-view';
+const prodSetting =
+    process.env.NODE_ENV === 'production'
+        ? {
+              publicPath: `/${prodDir}`,
+              outputDir: `${prodDir}`,
+          }
+        : {};
 module.exports = {
     // transpileDependencies: ['vue-echarts', 'resize-detector'],
     css: {
@@ -21,13 +30,11 @@ module.exports = {
                     'cell-background-color': '#0D1B49',
                     'cell-vertical-padding': '8px',
                     'dropdown-menu-title-font-size': '14px',
-                    'grid-item-content-background-color':'#1C2A5A'
+                    'grid-item-content-background-color': '#1C2A5A',
                 },
             },
         },
     },
-
     productionSourceMap: false,
-    publicPath: '/integrated-manage-mobile',
-    outputDir: 'integrated-manage-mobile'
+    ...prodSetting,
 };
